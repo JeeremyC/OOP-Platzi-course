b9edad993ddfc2a6afcb297466db8643aefd1fe9
JeeremyC <jeremyzetas@gmail.com>
2022-01-30 19:59:22 -0500

    Creamos un segundo objeto en java :D

--- Java/Main.java
+++ Java/Main.java
@@ -8,6 +8,12 @@ class Main {
         michi.size = "small";
         michi.clinical_history = "This cat was in the outside";
         michi.race = "black";
-        System.out.println("size: " + michi.size);
+        System.out.println("michi size: " + michi.size);
+
+        pets luna = new pets();
+        luna.age = 2;
+        luna.size = "medium";
+        luna.clinical_history = "any";
+        luna.race = "black & white";
     }
 }
