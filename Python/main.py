2968988282776c9f3f671e1e2080ee7e18d2630b
JeeremyC <jeremyzetas@gmail.com>
2022-01-30 20:11:55 -0500

    Creamos un objeto en python

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,11 @@
+from pets import Pets
+
 if __name__ == "__main__":
     print("holaaa")
+    
+    michi = Pets() #Creamos un objeto michi llamando al metodo constructor Pets()
+    michi.age = 2   #Asignamos valores a las variables
+    michi.race = "Siberian"
+    michi.size = "small"
+    michi.clinical_history = "unknow"
+    print(vars(michi)) #imprimimos las variables en formato JSON
